f8126efec657cb12bd5e221c698fc050e078afb7
OKh <hudoshin7605@gmail.com>
2025-12-04 18:35:50 +0100

    Frontend: fix impressum.html

---
+++ frontend/js/partials-loader.js
@@ -0,0 +1,11 @@
+/* global toggleMobileMenu */
+
+async function loadPartials() {
+    const [head, foot] = await Promise.all([
+        fetch('../../partials/header.html').then(r => r.text()),
+        fetch('../../partials/footer.html').then(r => r.text())
+    ]);
+    document.body.insertAdjacentHTML('afterbegin', head);
+    document.body.insertAdjacentHTML('beforeend', foot);
+}
+loadPartials().catch(console.error);
